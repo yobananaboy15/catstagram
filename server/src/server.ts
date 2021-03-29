d0c3feea5df99fce898ad0cc67177ee6175aa9f6
Axel Thiel <axth2220@gmail.com>
2021-03-29 17:48:27 +0200

    minor changes

---
+++ server/src/server.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+//Importera mina routes.
+
+const app = express();
+dotenv.config();
+
+//Sätt upp all middleware. Kan behöva lägga till options för att öka storleken på filerna.
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
+//app.use(mina routes)
+
+//Koppla upp sig mot databasennsssss
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(`${process.env.CONNECTION_URL}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+})
+.then(() =>
+    app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
