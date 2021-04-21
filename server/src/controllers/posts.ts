08523a8212dd8018d42d0305cdb4c0ccb5c157ce
Axel Thiel <axth2220@gmail.com>
2021-04-21 13:41:51 +0200

    minor changes

--- server/src/controllers/posts.ts
+++ server/src/controllers/posts.ts
@@ -1,29 +1,27 @@
 import Post from "../models/post";
-import {Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from "express";
 
 type routeHandler = (
-    req: Request,
-    res: Response,
-    next?: NextFunction
-) => unknown
+  req: Request,
+  res: Response,
+  next?: NextFunction
+) => unknown;
 
 export const getPosts: routeHandler = async (req, res) => {
-    try {
-        const posts = await Post.find()
-        console.log(posts)
-        res.status(200).json(posts)
-    } catch (error) {
-        
-    }
-}
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {}
+};
 
 export const addPost: routeHandler = async (req, res) => {
-    const post = req.body;
-    const newPost = new Post(post)
-    try {
-        await newPost.save()
-        res.status(201).json(newPost)
-    } catch (error) {
-        res.status(409).json(error)
-    }
-}
+  const post = req.body;
+  console.log(post);
+  const newPost = new Post(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json(error);
+  }
+};
--- server/src/models/post.ts
+++ server/src/models/post.ts
@@ -1,20 +1,20 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
-    description: String,
-    creator: String,
-    tags: String,
-    imgURL: String,
-    likeCount: {
-      type: Number,
-      default: 0,
-    },
-    createdAt: {
-      type: Date,
-      default: new Date(),
-    },
-})
+  description: String,
+  creator: String,
+  tags: String,
+  imgURL: { type: String, required: true },
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
 
-const Post = mongoose.model('Post', postSchema)
+const Post = mongoose.model("Post", postSchema);
 
 export default Post;
