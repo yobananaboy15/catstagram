7954024ad3d389454f364ca29730e04458c3270c
Axel Thiel <axth2220@gmail.com>
2021-05-04 09:48:34 +0200

    removed comments

--- client/src/components/Posts.tsx
+++ client/src/components/Posts.tsx
@@ -4,14 +4,8 @@ import { getPosts } from "../api/index";
 import { Post } from "./Post";
 import { PostsContext } from "../contexts/PostsContext";
 
-// interface Post {
-//   _id: object;
-//   description?: string;
-//   tags?: string;
-//   imgURL: string;
-// }
 export const Posts = () => {
-  const { posts, setPosts } = useContext(PostsContext); //Hämta detta från context
+  const { posts, setPosts } = useContext(PostsContext);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -24,8 +18,6 @@ export const Posts = () => {
   }, []);
 
   return (
-    //Gör om till post component
-    //Tre tillstånd, 1: Det finns inga poster efter hämtning. posts = [], 2. Den laddar datan. 3. Det finns poster och dom är hämtade.
     <Grid container>
       {isLoading ? (
         <CircularProgress />
